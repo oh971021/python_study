20770a6fa3cd2f4c023d7fb02892edbc78493756
oh971021 <oh971021@naver.com>
2021-07-06 17:51:22 +0900

    study

--- study-2/main.py
+++ study-2/main.py
@@ -1,34 +1,9 @@
-import numpy as np
- 
-a = np.zeros((2,2))
-print(a)
-# 출력:
-# [[ 0.  0.]
-#  [ 0.  0.]]
- 
-a = np.ones((2,3))
-print(a)
-# 출력:
-# [[ 1.  1.  1.]
-#  [ 1.  1.  1.]]
- 
-a = np.full((2,3), 5)
-print(a)
-# 출력:
-# [[5 5 5]
-#  [5 5 5]]
- 
-a = np.eye(3)
-print(a)
-# 출력:
-# [[ 1.  0.  0.]
-#  [ 0.  1.  0.]
-#  [ 0.  0.  1.]]
- 
-a = np.array(range(20)).reshape((4,5))
-print(a)
-# 출력:
-# [[ 0  1  2  3  4]
-#  [ 5  6  7  8  9]
-#  [10 11 12 13 14]
-#  [15 16 17 18 19]]
+# 랜덤모듈을 사용한 확률문 만들기
+
+import random
+
+if random.randint(0, 1) :
+    print('저녁 먹는다')
+else:
+    print("안 먹는다")
+
