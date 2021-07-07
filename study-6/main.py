22684e059ae61f52dae999a463a39f8ccbe9f5ca
Junseok <oh971021@gmail.com>
2021-07-08 01:10:43 +0900

    study

---
+++ study-6/main.py
@@ -0,0 +1,17 @@
+import time
+
+first = time.time()
+while first + 5 >= time.time():
+    pass
+print("-" * 50)
+
+# 배열 하나로 출력해보기
+list = [
+  [1,2,3],
+  [4,5,6,7],
+  [8,9],
+]
+
+for i in list :
+  for j in i :
+    print(j)
