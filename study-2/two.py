37c001404bab54bff4705444f84f7bb8ca632817
Junseok <oh971021@gmail.com>
2021-07-09 01:55:26 +0900

    study

---
+++ study-2/two.py
@@ -0,0 +1,7 @@
+import random
+
+while True :
+  i = random.randint(0,40)
+  if i == 2 :
+    break
+  print(i, end='\n')
---
+++ study-6/forstudy.py
@@ -0,0 +1,7 @@
+for i in range(1, 10, 1) :
+  print("-" * 2, "{}".format(i),"단", "-" * 2)
+  for j in range(1, 10, 1) :
+    if j == 5 :
+      pass
+    else : 
+      print("{} * {} = {}".format(i, j, i*j))
