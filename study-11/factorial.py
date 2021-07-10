6b6bd3033e56c6b343e724b512d6c6b967b6ef7e
Junseok <oh971021@gmail.com>
2021-07-10 20:15:19 +0900

    study

--- study-11/factorial.py
+++ study-11/factorial.py
@@ -3,13 +3,19 @@
 # n! = n * (n-1)
 # 0! = 1
 
+import time
+
 print("-- 팩토리얼 --")
+
+start_time = time.time()
 
 def factorial_1(n):
   value = 1
   for i in range(1, n + 1):
     value *= i
   return value
+
+end_time = time.time()
 
 # 재귀함수
 def factorial_2(n):
@@ -18,7 +24,7 @@ def factorial_2(n):
   else:
     return n * factorial_2(n-1)
 
-print(factorial_1(5))
-print(factorial_1(10))
+print(factorial_1(7))
+print(time.time() - start_time)
 
 print("-" * 14)
