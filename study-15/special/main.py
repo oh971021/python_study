7626ed3a0ca55b04410a93b27eab9131d3fedc94
Junseok <oh971021@gmail.com>
2021-08-01 00:02:43 +0900

    7월 31일 Class_mainfunc

--- study-15/special/main.py
+++ study-15/special/main.py
@@ -1,20 +1,20 @@
 class Klass :
-  # 이 친구는 스페셜 메소드
-  # 생성자 메소드
-  def __new__(cls, name, age) :
-    print("객체가 생성되었습니다")
-    return object.__new__(cls)
+    # 이 친구는 스페셜 메소드
+    # 생성자 메소드
+    def __new__(cls, name, age) :
+        print("객체가 생성되었습니다")
+        return object.__new__(cls)
 
-  # 초기화 메소드
-  def __init__(self, name, age) :
-    print("객체를 초기화 합니다")
-    self.name = name
-    self.age = age
+    # 초기화 메소드
+    def __init__(self, name, age) :
+        print("객체를 초기화 합니다")
+        self.name = name
+        self.age = age
 
-  # 소멸자 메소드
-  def __del__(self) :
-    print("객체를 삭제합니다")
-    del self
-
-
-k = Klass("준석", 25)
+    # 소멸자 메소드
+    def __del__(self) :
+        print("객체를 삭제합니다")
+        del self
+        
+if __name__ == "__main__" :
+    k = Klass("준석", 25)
